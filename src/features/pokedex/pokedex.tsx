52a71bdf14e33f046a9d8d1c6c1ff4f38343675b
kylegrabski <kyle.grabski@gmail.com>
2023-05-01 18:12:47 -0700

    add try catches

--- src/features/pokedex/pokedex.tsx
+++ src/features/pokedex/pokedex.tsx
@@ -20,17 +20,18 @@ export function Pokedex() {
     const fetchPokemon = async () => {
         try {
             // Initial fetch to get the first 151 Pokemon
-            const {data} = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
-            console.log(data);
-
+            const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
             const pokemonArray: IPokemonData[] = [];
 
             // Loop through the first 151 Pokemon and get the unique data for each.
             // Then push it into an array.
             for (const pokemon of data.results) {
-                const res = await fetch(pokemon.url);
-                const item = await res.json();
-                pokemonArray.push(item);
+                try {
+                    const { data: pokemonData } = await axios.get(pokemon.url);
+                    pokemonArray.push(pokemonData);
+                } catch (error) {
+                    throw new Error(`Failed to fetch data for ${pokemon.name}: ${error}`);
+                }
             }
             console.log(pokemonArray);
             setPokemon(pokemonArray);
@@ -40,7 +41,7 @@ export function Pokedex() {
     };
 
     const getAPI = async () => {
-        const response = await axios.get("http://localhost:5226/")
+        const response = await axios.get("http://localhost:5226")
         console.log(`HERES THE RESPONSE!! `, response);
     }
 
@@ -53,9 +54,6 @@ export function Pokedex() {
                     <p>Entry number: {item.id}</p>
                     <p>Name: {item.name}</p>
                     <img src={item.sprites.front_default} alt={item.name} />
-                    <img src={item.sprites.front_shiny} alt={item.name} />
-                    <img src={item.sprites.back_default} alt={item.name} />
-                    <img src={item.sprites.back_shiny} alt={item.name} />
                 </div>
             ))}
         </>
