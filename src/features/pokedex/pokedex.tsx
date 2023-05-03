ecff8f192cd8fd613306c148c4352db97a726742
kylegrabski <kyle.grabski@gmail.com>
2023-05-02 22:12:01 -0700

    WIP pagination scroll

--- src/features/pokedex/pokedex.tsx
+++ src/features/pokedex/pokedex.tsx
@@ -13,28 +13,47 @@ interface IPokemonData {
 
 export function Pokedex() {
     const [pokemon, setPokemon] = useState<IPokemonData[]>([]);
+    const [page, setPage] = useState<number>(1);
 
     useEffect(() => {
         getPokemon()
     }, [])
 
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+      }, []);
+
+    function handleScroll() {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
+          setPage(page + 1);
+        console.log(`WE HIT IT: ${page}`);
+        }
+    }
+    
+    // @TODO Call getPokemon when new page State gets sets
     const getPokemon = async () => {
         try {
+            console.log(`YO`);
             const sh = new StorageHelper("All-Pokemon");
 
-            if (!sh.get()) {
-                const url: string = process.env.REACT_APP_GET_ALL_POKEMON || "";
-                if (!url) {
-                    throw new Error("NO ENV FOUND")
-                }
-                const { data } = await axios.get(url)
-                setPokemon(data);
-                sh.save(data);
-                return
+            // if (!sh.get()) {
+            const url: string = process.env.REACT_APP_GET_ALL_POKEMON || "";
+            if (!url) {
+                throw new Error("NO ENV FOUND")
             }
+            const { data } = await axios.get(`${url}/1/51`)
+            setPokemon(data);
 
-            const pokemonArray = sh.get();
-            setPokemon(pokemonArray);
+            if (sh.get().length != pokemon.length) {
+                console.log(`WE GONNA SAVE IT!`);
+                sh.save(pokemon);
+            }
+            
+            // }
+
+            // const pokemonArray = sh.get();
+            // setPokemon(pokemonArray);
 
         } catch (error) {
             throw new Error(`failed to fetch Pokemon data: ${error}`);
