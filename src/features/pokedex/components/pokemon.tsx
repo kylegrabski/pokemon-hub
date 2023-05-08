3f9ba772b5f5051a891c7cf8f8f99bada86a0a98
kylegrabski <kyle.grabski@gmail.com>
2023-05-07 22:07:50 -0700

    abstract logic

--- src/features/pokedex/components/pokemon.tsx
+++ src/features/pokedex/components/pokemon.tsx
@@ -1,45 +1,16 @@
-// Import Libraries
-import { useState } from 'react';
-
 // Import Modules
 import './style.css';
 import { IPokemonData } from '../../../types/index';
 
 
-export function Pokemon({allPokemon, search}: {allPokemon: IPokemonData[], search: string}) {
-    const [expandedPokemon, setExpandedPokemon] = useState<IPokemonData>();
+export function Pokemon({allPokemon, search, handleExpandedView}: {allPokemon: IPokemonData[], search: string, handleExpandedView: any}) {
 
     const filteredPokemon = allPokemon.filter((element: IPokemonData) => element.name.toLowerCase().includes(search));
 
-    const expandedView = (item: IPokemonData) => {
-        if (expandedPokemon === item) {
-            setExpandedPokemon(undefined);
-            return;
-        }
-        setExpandedPokemon(item);
-    }
-
     return (
         <>
-            {expandedPokemon ? (
-                <div>
-                    <p>Entry number: {expandedPokemon.id}</p>
-                    <p>Name: {expandedPokemon.name}</p>
-                    <p>Weight: {expandedPokemon.weight} lbs</p>
-                    <p>Types: {expandedPokemon.types.map(item => {
-                        return <span key={item.type.name}>{item.type.name} </span>
-                    })}
-                    </p>
-                    <img src={expandedPokemon.sprites.front_default} alt={expandedPokemon.name} />
-                    {/* TEMPORARY "X" OUT BUTTON */}
-                    <button onClick={() => setExpandedPokemon(undefined)}>X</button>
-                </div>
-            ) : (
-                <p>CLICK POKEMON TO SHOW MORE STATS HERE</p>
-            )}
-
             {filteredPokemon.map((item: IPokemonData, index: number) => (
-                <div className='pokemon-card' key={index} onClick={() => expandedView(item)}>
+                <div className='pokemon-card' key={index} onClick={() => handleExpandedView(item)}>
                     <hr />
                     <p>Entry number: {item.id}</p>
                     <p>Name: {item.name}</p>
