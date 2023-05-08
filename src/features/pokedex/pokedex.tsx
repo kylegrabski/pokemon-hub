a4bb719107b8e2b4cd4d8dd82c89764c5ccf9d39
kylegrabski <kyle.grabski@gmail.com>
2023-05-07 17:11:45 -0700

    bring in search logic

--- src/features/pokedex/pokedex.tsx
+++ src/features/pokedex/pokedex.tsx
@@ -1,10 +1,41 @@
+// Import Libraries
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+// Import Modules
 import { Pokemon } from './components/pokemon';
+import { IPokemonData } from '../../types/index';
+
 
 export function Pokedex() {
- 
+    const [allPokemon, setAllPokemon] = useState<IPokemonData[]>([]);
+    const [search, setSearch] = useState<string>("");
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value.toLowerCase());
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        getPokemon()
+    }, [])
+
+    const getPokemon = async () => {
+        try {
+            const url: string = process.env.REACT_APP_GET_ALL_POKEMON || "";
+
+            if (!url) {
+                throw new Error("NO ENV FOUND")
+            }
+            const { data } = await axios.get(`${url}`)
+            setAllPokemon(data);
+        } catch (error) {
+            throw new Error(`failed to fetch Pokemon data: ${error}`);
+        }
+    };
+
     return (
         <>
-           <Pokemon/>
+            <input type="text" placeholder="Search.." onChange={handleSearch} />
+            <Pokemon allPokemon={allPokemon} search={search} />
         </>
     )
 }
