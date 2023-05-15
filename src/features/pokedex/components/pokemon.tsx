298d213531dac6cec1032541dd27cc42173cada4
kylegrabski <kyle.grabski@gmail.com>
2023-05-14 21:31:45 -0700

    animate sprite on hover

--- src/features/pokedex/components/pokemon.tsx
+++ src/features/pokedex/components/pokemon.tsx
@@ -1,6 +1,7 @@
 // Import Modules
 import './style.css';
 import { PokemonData } from '../../../types/index';
+import { useState } from 'react';
 
 interface PokemonProps {
     allPokemon: PokemonData[];
@@ -8,18 +9,29 @@ interface PokemonProps {
     addExpandedPokemon: (item: PokemonData) => void;
 }
 
-export function Pokemon({allPokemon, search, addExpandedPokemon}: PokemonProps) {
+
+export function Pokemon({ allPokemon, search, addExpandedPokemon }: PokemonProps) {
+    const [hoveredItemId, setHoveredItemId] = useState<number | null>(null);
 
     const filteredPokemon = allPokemon.filter((element: PokemonData) => element.name.toLowerCase().includes(search));
+
+    const toggleHover = (id: number) => {
+        setHoveredItemId(id);
+    }
 
     return (
         <>
             {filteredPokemon.map((item: PokemonData) => (
-                <div className='pokemon-card' key={item.id} onClick={() => addExpandedPokemon(item)}>
+                <div className='pokemon-card' key={item.id} onClick={() => addExpandedPokemon(item)} onMouseEnter={() => toggleHover(item.id)} onMouseLeave={() => toggleHover(0)}>
                     <hr />
                     <p>Entry number: {item.id}</p>
                     <p>Name: {item.name}</p>
-                    <img src={item.sprites.front_default} alt={item.name} />
+                    <div >
+                        <img
+                            src={item.id === hoveredItemId ? item.sprites.versions['generation-v']['black-white'].animated.front_default : item.sprites.front_default}
+                            alt={item.name}
+                        />
+                    </div>
                 </div>
             ))}
         </>
