141ab9ec782770c1f422a27e4f2af6ec14bbb154
kylegrabski <kyle.grabski@gmail.com>
2023-05-15 18:08:30 -0700

    basic style/grid/cards

--- src/features/pokedex/components/pokemon.tsx
+++ src/features/pokedex/components/pokemon.tsx
@@ -1,3 +1,11 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
 // Import Modules
 import './style.css';
 import { PokemonData } from '../../../types/index';
@@ -21,19 +29,44 @@ export function Pokemon({ allPokemon, search, addExpandedPokemon }: PokemonProps
 
     return (
         <>
-            {filteredPokemon.map((item: PokemonData) => (
-                <div className='pokemon-card' key={item.id} onClick={() => addExpandedPokemon(item)} onMouseEnter={() => toggleHover(item.id)} onMouseLeave={() => toggleHover(0)}>
-                    <hr />
-                    <p>Entry number: {item.id}</p>
-                    <p>Name: {item.name}</p>
-                    <div >
-                        <img
-                            src={item.id === hoveredItemId ? item.sprites.versions['generation-v']['black-white'].animated.front_default : item.sprites.front_default}
-                            alt={item.name}
-                        />
-                    </div>
-                </div>
-            ))}
+            <Grid container spacing={2}>
+                {filteredPokemon.map((item: PokemonData) => (
+                    <Grid className='pokemon-card' item xs={12} sm={6} md={4} lg={3} key={item.id} onClick={() => addExpandedPokemon(item)} onMouseEnter={() => toggleHover(item.id)} onMouseLeave={() => toggleHover(0)}>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardMedia
+                                className='pokemon-image'
+                                component="img"
+                                alt={item.name}
+                                height="140"
+                                image={item.id === hoveredItemId ? item.sprites.versions['generation-v']['black-white'].animated.front_default : item.sprites.front_default}
+                            />
+                            <CardContent>
+                                <Typography variant="body2" color="text.secondary">
+                                    #{item.id}
+                                </Typography>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {item.name}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Select Favorite</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </>
     )
 }
+
+/* // <div className='pokemon-card' key={item.id} onClick={() => addExpandedPokemon(item)} onMouseEnter={() => toggleHover(item.id)} onMouseLeave={() => toggleHover(0)}>
+//     <hr />
+//     <p>Entry number: {item.id}</p>
+//     <p>Name: {item.name}</p>
+//     <div >
+//         <img
+//             src={item.id === hoveredItemId ? item.sprites.versions['generation-v']['black-white'].animated.front_default : item.sprites.front_default}
+//             alt={item.name}
+//         />
+//     </div>
+// </div> */
