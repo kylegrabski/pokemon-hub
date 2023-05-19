fb5895190458faf9691cf9ecc4eaaf7685c3c7f2
kylegrabski <kyle.grabski@gmail.com>
2023-05-18 22:43:42 -0700

    remove unused params

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react';
 import { NavBar } from './features/navBar/navBar';
 import { Pokedex } from './features/pokedex/pokedex';
-// import './App.css';
 
 function App() {
-  const [search, setSearch] = useState<string>("");
-
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value.toLowerCase());
   return (
     <div className="App">
-      <NavBar handleSearch={handleSearch}/>
+      <NavBar/>
       <Pokedex/>
     </div>
   );
