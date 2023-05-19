6685b1ae5781f1918464e787cab34c6f21ae7811
kylegrabski <kyle.grabski@gmail.com>
2023-05-18 22:40:20 -0700

    format

--- src/features/pokedex/pokedex.tsx
+++ src/features/pokedex/pokedex.tsx
@@ -1,72 +1,72 @@
 // Import Libraries
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 // Import Modules
-import StorageHelper from '../../utils/storage';
-import { PokemonData } from '../../types/index';
+import StorageHelper from "../../utils/storage";
+import { PokemonData } from "../../types/index";
 
 // Import Components
-import { Pokemon } from './components/pokemon';
-import { NavBar } from './components/navBar';
-import { ExpandedPokemonView } from './components/expandedPokemonView';
+import { Pokemon } from "./components/pokemon";
+import { ExpandedPokemonView } from "./components/expandedPokemonView";
 
 // Import Store
-import { useAppSelector } from '../../app/hooks';
-// import { selectSearch } from '../navBar/searchSlice';
-
+import { useAppSelector } from "../../app/hooks";
+import { selectSearch } from "../navBar/searchSlice";
 
 export function Pokedex() {
-    const [allPokemon, setAllPokemon] = useState<PokemonData[]>([]);
-    const [expandedPokemon, setExpandedPokemon] = useState<PokemonData | undefined>();
-    const [search, setSearch] = useState<string>("");
+  const searchPokemon = useAppSelector(selectSearch);
 
-    const addExpandedPokemon = (item: PokemonData) : void => {
-        if (expandedPokemon === item) {
-            setExpandedPokemon(undefined);
-            return;
-        }
-        setExpandedPokemon(item);
+  const [allPokemon, setAllPokemon] = useState<PokemonData[]>([]);
+  const [expandedPokemon, setExpandedPokemon] = useState<PokemonData | undefined>();
+
+  const addExpandedPokemon = (item: PokemonData): void => {
+    if (expandedPokemon === item) {
+      setExpandedPokemon(undefined);
+      return;
     }
+    setExpandedPokemon(item);
+  };
 
-    const removeExpandedPokemon = () => setExpandedPokemon(undefined);
-    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value.toLowerCase());
+  const removeExpandedPokemon = () => setExpandedPokemon(undefined);
 
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
+  const getPokemon = async () => {
+    try {
+      const sh = new StorageHelper("AllPokemon");
+
+      const cachedPokemon = sh.get();
+      if (cachedPokemon) {
+        setAllPokemon(cachedPokemon);
+        return;
+      }
+
+      const url: string = process.env.REACT_APP_GET_ALL_POKEMON || "";
+      if (!url) {
+        throw new Error("NO ENV FOUND");
+      }
+      const { data } = await axios.get(`${url}`);
+      sh.set(data);
+      setAllPokemon(data);
+    } catch (error) {
+      throw new Error(`failed to fetch Pokemon data: ${error}`);
     }
+  };
 
-    useEffect(() => {
-        getPokemon()
-    }, [])
-
-    const getPokemon = async () => {
-        try {
-            const sh = new StorageHelper("AllPokemon");
-
-            const cachedPokemon = sh.get();
-            if (cachedPokemon) {
-                setAllPokemon(cachedPokemon);
-                return;
-            }
-
-            const url: string = process.env.REACT_APP_GET_ALL_POKEMON || "";
-            if (!url) {
-                throw new Error("NO ENV FOUND")
-            }
-            const { data } = await axios.get(`${url}`)
-            sh.set(data);
-            setAllPokemon(data);
-        } catch (error) {
-            throw new Error(`failed to fetch Pokemon data: ${error}`);
-        }
-    };
-    
-    
-    return (
-        <>
-            <NavBar handleSearch={handleSearch}/>
-            <ExpandedPokemonView expandedPokemon={expandedPokemon} removeExpandedPokemon={removeExpandedPokemon}/>
-            <Pokemon allPokemon={allPokemon} search={search} addExpandedPokemon={addExpandedPokemon} />
-        </>
-    )
+  return (
+    <>
+      <ExpandedPokemonView
+        expandedPokemon={expandedPokemon}
+        removeExpandedPokemon={removeExpandedPokemon}
+      />
+      <Pokemon
+        allPokemon={allPokemon}
+        search={searchPokemon}
+        addExpandedPokemon={addExpandedPokemon}
+      />
+    </>
+  );
 }
